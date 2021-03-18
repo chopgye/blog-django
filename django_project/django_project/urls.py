1bb816f3f2cb1ec307472535643fabbae1fe813a
vagabond <vagabond@DESKTOP-AGVK968.localdomain>
2021-03-17 23:11:31 -0400

    login authentication
    added login, logout and profile funtionality
    fixed some edge case where user can access info without being logged in

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -6,9 +6,13 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'), 
+    path('admin/', admin.site.urls),   
     path('', include('blog.urls')),   #only sends '' to blog.urls
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), 
+    path('profile/', user_views.profile, name='profile'), 
 ]
